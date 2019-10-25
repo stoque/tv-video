a98fcb25f1c1df68dffc657cc29e4c204723f9dc
Lucas Stoque <stoque@live.com>
2019-10-24 23:31:53 -0300

    feat(home): add page

--- src/pages/splash/splash.js
+++ src/pages/splash/splash.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
+import styles from './splash.module.scss'
+
 function Splash () {
   const history = useHistory()
 
   useEffect(() => {
     setInterval(() => {
       history.push('/login')
-    }, 2000)
+    }, 30000)
   }, [history])
 
   return (
-    <h1>Splash</h1>
+    <h1 className={styles.splash}>Splash</h1>
   )
 }
 
